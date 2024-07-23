d5b926f30bd4fafd74636402a53f95b948ca95cc
jayhong329 <smoja329@gmail.com>
2024-07-23 21:02:26 +0800

    locals練習

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from myapp import views
 
 urlpatterns = [
-    # path('比對路徑')
+    # path('比對路徑', '要執行的function')
     path('', views.index),  #http://127.0.0.1:8000/store/
+    path('details/<uuid:product_id>', views.details),
+    path('about/', views.about),  #http://127.0.0.1:8000/store/about/
+    path('about/<int:year>', views.about),
+    path('blog/<path:publish>', views.blog), 
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
+
 # Create your views here.
 def index(request):
-    return HttpResponse('<h2>我的APP 首頁</h2>')
+    title = '我的商店 首頁'
+    now = datetime.now()
+    id = '123e4567-e89b-12d3-a456-426655440000'
+
+    return render(request, 'myapp/index.html', locals())
+
+
+
+    # return HttpResponse('<h2>我的APP 首頁</h2>')
+
+def details(request, producr_id=''):
+    return HttpResponse('<h2>讀出商品編號 {producr_id}的商品</h2>')
+
+def about(request, year= datetime.now().year):
+    return HttpResponse(f'<h2> ABOUT {year} </h2>')
+
+# publisg => 2024/07/23
+def blog(request, publish = None):
+    return HttpResponse(f'<h2>讀取 {publish} 的文章 </h2>')
