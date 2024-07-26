60646634244c90f7278b21a8b71cf8ec4163fe0b
jayhong329 <smoja329@gmail.com>
2024-07-26 21:49:23 +0800

    主版連結置換

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from myapp import views
 
+app_name = 'myapp'
 urlpatterns = [
     # path('比對路徑', '要執行的function')
-    path('', views.index),  #http://127.0.0.1:8000/store/
+    path('', views.index, name='index'),  #http://127.0.0.1:8000/store/
     path('details/<uuid:product_id>', views.details),
-    path('about/', views.about),  #http://127.0.0.1:8000/store/about/
+    path('about/', views.about, name = 'about'),  #http://127.0.0.1:8000/store/about/
     path('about/<int:year>', views.about),
     path('blog/<path:publish>', views.blog),
-    path ('show', views.show)
+    path ('show', views.show, name= 'show')
 ]
