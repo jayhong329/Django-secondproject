a6d8b31d76ce04e3f6a81749b292a7d1f6931732
jayhong329 <smoja329@gmail.com>
2024-08-01 20:53:36 +0800

    嘗試CodeFirst

--- member/models.py
+++ member/models.py
@@ -11,6 +11,14 @@ class Member(models.Model):
     member_address = models.CharField(max_length=200, null=True)
     last_update = models.DateTimeField(default=datetime.now())
 
+class Member_log(models.Model):
+    member_id = models.IntegerField(primary_key=True)
+    member_login = models.IntegerField(unique=True)
+    provider = models.CharField(max_length=50)
+    provider_id_google = models.CharField(max_length=50)
+    provider_id_line = models.CharField(max_length=50)
+    provider_id_fb = models.CharField(max_length=50)
+    access_token = models.CharField(max_length=50)
 
 class Meta:
     ds_table = 'member'  # 指定的資料表名稱
