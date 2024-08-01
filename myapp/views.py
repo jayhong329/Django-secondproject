a1d4ea85f977cc9a3a41c1a62f97bc4005c202e9
jayhong329 <smoja329@gmail.com>
2024-08-01 19:54:20 +0800

    django.db.connection使用範例

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from .models import Category
 
 # Create your views here.
 def index(request):
@@ -32,4 +33,11 @@ def blog(request, publish = None):
     return HttpResponse(f'<h2>讀取 {publish} 的文章 </h2>')
 
 def show(request):
-    return render(request, 'myapp/show.html')
+    title = "資料庫讀取"
+    # 跟 Model要資料
+    categories = Category.all()
+    print(categories)
+
+    #把資料傳給 templates 透過 render()的第三個參數，傳入一個{}結構的資料
+    return render(request, 'myapp/show.html', locals())
+
