a1f489414beb4e41a1ade6586bdde44ad927fea6
jayhong329 <smoja329@gmail.com>
2024-08-08 20:48:32 +0800

    cookie 與 session 的讀取與寫入

--- member/urls.py
+++ member/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('register/', views.register, name="create" ),   # http://127.0.0.1:8000/member/register
     path('mobile/', views.mobile, name="mobile" ),   # http://127.0.0.1:8000/member/mobile
     path('formdemo/', views.formdemo, name="formdemo" ),   # http://127.0.0.1:8000/member/formdemo
+    path('write/', views.write, name="write" ),   # http://127.0.0.1:8000/member/write
+    path('read/', views.read, name="read" ),   # http://127.0.0.1:8000/member/read
 
 ]
